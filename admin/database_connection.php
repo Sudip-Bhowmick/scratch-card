8ef865b6bfe0120ee77d278155ec18bf20438c4c
SudipBhowmick <71332388+SudipBhowmick@users.noreply.github.com>
2021-12-19 18:51:20 +0530

    first commit

---
+++ admin/database_connection.php
@@ -0,0 +1,7 @@
+<?php
+
+//database_connection.php
+
+$connect = new PDO('mysql:host=localhost;dbname=sudipbho_scratchcard', 'sudipbho_wp520', 'mieVr8LL9cq4B!E');
+
+?>
---
+++ sudipbho_scratchcard.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 17, 2021 at 10:27 PM
+-- Server version: 5.7.35-cll-lve
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sudipbho_scratchcard`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_image`
+--
+
+CREATE TABLE `tbl_image` (
+  `image_id` int(11) NOT NULL,
+  `image_name` varchar(250) NOT NULL,
+  `image_description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_image`
+--
+
+INSERT INTO `tbl_image` (`image_id`, `image_name`, `image_description`) VALUES
+(20, '10.jpg', ''),
+(21, '11.jpg', ''),
+(22, '12.jpg', ''),
+(31, '01.jpg', ''),
+(32, '02.jpg', ''),
+(33, '03.jpg', ''),
+(34, '04.jpg', ''),
+(35, '06.jpg', ''),
+(36, '07.jpg', ''),
+(37, '08.jpg', ''),
+(38, '09.jpg', ''),
+(39, '13.jpg', ''),
+(40, '05.jpg', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
